7da300f89e37a9310ea1f1ab908d00a610a8e90f
Dev <devendra08440@gmail.com>
2023-11-16 14:40:55 +0530

    Added: Products

---
+++ frontend/src/Components/Data/Data.js
@@ -0,0 +1,41 @@
+import p1 from './Images/1.jpg';
+import p2 from './Images/2.jpg';
+import p3 from './Images/3.jpg';
+import p4 from './Images/4.jpg';
+
+let Products = [
+    {
+        id: 1,
+        brand: 'Levi\'s',
+        name: 'Men Regular Fit Checkered Spread Collar Casual Shirt',
+        image: p1,
+        new_price: 1991,
+        old_price: 2399
+    },
+    {
+        id: 2,
+        brand: 'Levi\'s',
+        name: 'Men Slim Fit Solid Spread Collar Casual Shirt',
+        image: p2,
+        new_price: 1899,
+        old_price: 2599
+    },
+    {
+        id: 3,
+        brand: 'Levi\'s',
+        name: 'Men Slim Fit Solid Spread Collar Casual Shirt',
+        image: p3,
+        new_price: 2821,
+        old_price: 3399
+    },
+    {
+        id: 4,
+        brand: 'Levi\'s',
+        name: 'Men Slim Fit Printed Spread Collar Casual Shirt',
+        image: p4,
+        new_price: 1249,
+        old_price: 2499
+    }
+];
+
+export default Products;
