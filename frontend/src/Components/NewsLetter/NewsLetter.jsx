341ec00e8f48c734bd215812649c01fea77a0aca
Dev <devendra08440@gmail.com>
2023-11-16 18:06:37 +0530

    Added: NewsLetter Component

---
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './NewsLetter.css';
+
+const NewsLetter = () => {
+    return (
+        <div className="news-letter">
+            <h1>Get Exclusive Offers On Your Email</h1>
+            <p>Subscribe to our News Letter and stay updated</p>
+
+            <div>
+                <input type="email" placeholder="Your Email ID" />
+                <button>Subscribe</button>
+            </div>
+        </div>
+    )
+}
+
+export default NewsLetter;
